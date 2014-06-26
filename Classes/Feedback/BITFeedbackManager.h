43a7fe98e50941ad5c90db59cf0adee561f721d3
Andreas Linde <mail@andreaslinde.de>
2014-06-26 15:21:39 +0200

    Use NS_ENUM
    - Also allows documentation support with appledoc

--- Classes/Feedback/BITFeedbackManager.h
+++ Classes/Feedback/BITFeedbackManager.h
@@ -39,11 +39,23 @@
 #define BITHockeyFeedbackMessagesLoadingFinished @"BITHockeyFeedbackMessagesLoadingFinished"
 
 
-typedef enum {
-  BITFeedbackUserDataElementDontShow = 0, // don't ask for this user data element at all
-  BITFeedbackUserDataElementOptional = 1, // the user may provide it, but does not have to
-  BITFeedbackUserDataElementRequired = 2 // the user has to provide this to continue
-} BITFeedbackUserDataElement;
+/**
+ *  Defines behavior of the user data field
+ */
+typedef NS_ENUM(NSInteger, BITFeedbackUserDataElement) {
+  /**
+   *  don't ask for this user data element at all
+   */
+  BITFeedbackUserDataElementDontShow = 0,
+  /**
+   *  the user may provide it, but does not have to
+   */
+  BITFeedbackUserDataElementOptional = 1,
+  /**
+   *  the user has to provide this to continue
+   */
+  BITFeedbackUserDataElementRequired = 2
+};
 
 
 @class BITFeedbackMessage;
