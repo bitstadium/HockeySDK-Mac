656ba39550e5657cdaffb8d9497389794a162f1d
Christoph Wendt <chwend@microsoft.com>
2016-03-10 15:41:59 -0800

    Add missing headers

--- Classes/CrashReporting/BITCrashReportTextFormatter.m
+++ Classes/CrashReporting/BITCrashReportTextFormatter.m
@@ -33,6 +33,9 @@
 
 #import "CrashReporter.h"
 
+#import "HockeySDK.h"
+#import "HockeySDKPrivate.h"
+
 #import <mach-o/dyld.h>
 #import <mach-o/getsect.h>
 #import <mach-o/ldsyms.h>
@@ -690,6 +693,30 @@ static const char *findSEL (const char *imageName, NSString *imageUUID, uint64_t
     return imageType;
 }
 
+/**
+ *  Remove the user's name from a crash's process path.
+ *  This is only necessary when sending crashes from the simulator as the path
+ *  then contains the username of the Mac the simulator is running on.
+ *
+ *  @param processPath A string containing the username
+ *
+ *  @return An anonymized string where the real username is replaced by "USER"
+ */
++ (NSString *)anonymizedProcessPathFromProcessPath:(NSString *)processPath {
+    
+    NSString *anonymizedProcessPath = [NSString string];
+    
+    if (([processPath length] > 0) && [processPath hasPrefix:@"/Users/"]) {
+        NSError *error = nil;
+        NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"(/Users/[^/]+/)" options:0 error:&error];
+        anonymizedProcessPath = [regex stringByReplacingMatchesInString:processPath options:0 range:NSMakeRange(0, [processPath length]) withTemplate:@"/Users/USER/"];
+        if (error) {
+            BITHockeyLog("ERROR: String replacing failed - %@", error.localizedDescription);
+        }
+    }
+    return anonymizedProcessPath;
+}
+
 @end
 
 
@@ -845,29 +872,4 @@ static const char *findSEL (const char *imageName, NSString *imageUUID, uint64_t
             lp64 ? 16 : 8, frameInfo.instructionPointer,
             symbolString];
 }
-
-/**
- *  Remove the user's name from a crash's process path.
- *  This is only necessary when sending crashes from the simulator as the path
- *  then contains the username of the Mac the simulator is running on.
- *
- *  @param processPath A string containing the username
- *
- *  @return An anonymized string where the real username is replaced by "USER"
- */
-+ (NSString *)anonymizedProcessPathFromProcessPath:(NSString *)processPath {
-    
-    NSString *anonymizedProcessPath = [NSString string];
-    
-    if (([processPath length] > 0) && [processPath hasPrefix:@"/Users/"]) {
-        NSError *error = nil;
-        NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"(/Users/[^/]+/)" options:0 error:&error];
-        anonymizedProcessPath = [regex stringByReplacingMatchesInString:processPath options:0 range:NSMakeRange(0, [processPath length]) withTemplate:@"/Users/USER/"];
-        if (error) {
-            BITHockeyLog("ERROR: String replacing failed - %@", error.localizedDescription);
-        }
-    }
-    return anonymizedProcessPath;
-}
-
 @end
