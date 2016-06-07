bdccaeb740318702d04fd6ffc297d64a2b177a97
Lukas Spieß <lspiess@microsoft.com>
2016-06-08 01:20:36 +0200

    Fix broken merge with duplicate session property

--- Classes/Telemetry/BITSender.m
+++ Classes/Telemetry/BITSender.m
@@ -16,12 +16,6 @@ static NSUInteger const BITDefaultRequestLimit = 10;
 
 @end
 
-@interface BITSender ()
-
-@property (nonatomic, strong) NSURLSession *session;
-
-@end
-
 @implementation BITSender
 
 @synthesize runningRequestsCount = _runningRequestsCount;
