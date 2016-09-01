0b2b3f2e6ef59b1467c8c19c36d2c95bad69f362
Benjamin Scholtysik (Reimold) <bereimol@microsoft.com>
2016-09-01 09:46:27 -0700

    Add API to track events with properties and measurements

--- Classes/Telemetry/BITMetricsManager.m
+++ Classes/Telemetry/BITMetricsManager.m
@@ -199,6 +199,25 @@ static NSString *const BITMetricsURLPathString = @"v2/track";
   });
 }
 
+- (void)trackEventWithName:(nonnull NSString *)eventName properties:(nullable NSDictionary<NSString *, NSString *> *)properties measurements:(nullable NSDictionary<NSString *, NSNumber *> *)measurements {
+  if (!eventName) { return; }
+  if (self.disabled) {
+    BITHockeyLogDebug(@"INFO: BITMetricsManager is disabled, therefore this tracking call was ignored.");
+    return;
+  }
+  
+  __weak typeof(self) weakSelf = self;
+  dispatch_async(self.metricsEventQueue, ^{
+    typeof(self) strongSelf = weakSelf;
+    BITEventData *eventData = [BITEventData new];
+    [eventData setName:eventName];
+    [eventData setProperties:properties];
+    [eventData setMeasurements:measurements];
+    [strongSelf trackDataItem:eventData];
+  });
+}
+
+
 #pragma mark Track DataItem
 
 - (void)trackDataItem:(BITTelemetryData *)dataItem {
